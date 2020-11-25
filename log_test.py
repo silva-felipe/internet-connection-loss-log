acae5c676c049465a3bf53f1f19ba4349084674e
silva-felipe <74065606+silva-felipe@users.noreply.github.com>
2020-11-25 11:35:49 -0300

    Add files via upload

---
+++ log_test.py
@@ -0,0 +1,35 @@
+import requests
+import schedule
+import smtplib
+from datetime import datetime
+
+log = []
+
+
+def internet():
+    global log
+    url = 'https://uol.com.br/'  # url to be tested
+    timeout = 5
+    usr = 's.felipe.almeida@gmail.com'  # your email address
+    pwd = 'omega000'  # your email pwd
+    data = datetime.today().strftime('%d/%m/%Y - %H:%M:%S')
+    try:
+        connection = requests.get(url, timeout=timeout)
+        if connection and log != []:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            msg = '''Subject: Connection lost\n
+Connection lost from {} to {}'''.format(log[0], log[-1])
+            server.login(usr, pwd)
+            server.sendmail(usr, usr, msg)
+            log = []
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        if exception:
+            log.append(data)
+            if len(log) == 3:
+                log.pop(1)
+
+
+schedule.every(5).seconds.do(internet)
+
+while __name__ == '__main__':
+    schedule.run_pending()
